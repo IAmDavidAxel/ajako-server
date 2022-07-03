d22656f73b9deef53f63696b1a547b65ec81a0ac
David Axel Nikyèma <david-axel-wendbenedo.nikyema.1@ulaval.ca>
2022-07-02 23:17:21 -0300

    Initial commit

---
+++ src/main/java/com/example/ajakoserver/AjakoServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ajakoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjakoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjakoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ajakoserver/AjakoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ajakoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjakoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
